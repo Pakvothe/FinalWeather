dd92e1e243da33c91506bf85700f36f14e824227
Pakvothe <franqitto@gmail.com>
2021-01-02 03:17:07 -0300

    minor fixes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ export default function About({ onClose }) {
 			<h4>About</h4>
 			<p className={styles.lorem}>This is a simple application created with React that uses the OpenWeather API to get the temperature of the city you want. </p>
 			<p className={styles.lorem}>Created and Developed by Franco Ortiz ~ 2020 </p>
-			<a className={styles.github} href="https://github.com/Pakvothe">Visit my Github!</a>
+			<a className={styles.github} href="https://github.com/Pakvothe/FinalWeather" target='_blank' rel='noreferrer'>Visit the Repository!</a>
 		</div>
 	);
 }
